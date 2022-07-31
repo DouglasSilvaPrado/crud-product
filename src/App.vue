0dda53eb021e5d8e5ce8b84644fe45871bf6764a
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2022-07-31 19:53:38 -0300

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
   import Navbar from './components/layout/Navbar.vue';
+  import Footer from './components/layout/Footer.vue';
 
 </script>
 
 <template>
   <Navbar />
   <router-view></router-view>
+  <Footer />
 </template>
 
---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,6 @@
+<template>
+  <!-- FOOTER -->
+  <footer class="bg-dark text-light text-center fixed-bottom py-3">
+    <div>2022 &copy; Douglas Prado</div>
+  </footer>
+</template>
